73af81258a1cb3b15ff5f5952f2e9e767ff8091c
Amol Gholap <agholap@gmail.com>
2018-01-23 15:05:32 -0800

    updated with app insight

--- TodoApp/GetData.cs
+++ TodoApp/GetData.cs
@@ -8,39 +8,52 @@ using Microsoft.Azure.WebJobs.Host;
 using System.Collections.Generic;
 using System.Collections;
 using System.Runtime.Serialization;
+using System;
+using Microsoft.ApplicationInsights;
 
 namespace TodoApp
 {
     public static class GetData
     {
+        public static ITelemetryClientFactory telemetryFactory = new TelemetryClientFactory();
         [FunctionName("GetData")]
         public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
         {
-            log.Info("C# HTTP trigger function processed a request.");
+            string name = ""; List<Contact> contacts = null;
+            TelemetryClient telemetryClient = telemetryFactory.GetClient();
+            try
+            {
+                log.Info("C# HTTP trigger function processed a request. - after function updated..");
 
-            // parse query parameter
-            string name = req.GetQueryNameValuePairs()
-                .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
-                .Value;
+                // parse query parameter
+                name = req.GetQueryNameValuePairs()
+                    .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
+                    .Value;
 
-            // Get request body
-            dynamic data = await req.Content.ReadAsAsync<object>();
+                // Get request body
+                dynamic data = await req.Content.ReadAsAsync<object>();
+                telemetryClient.TrackEvent("Inside GetData function");
+                // Set name to query string or body data
+                name = name ?? data?.name;
+                contacts = new List<Contact>();
 
-            // Set name to query string or body data
-            name = name ?? data?.name;
-            List<Contact> contacts = new List<Contact> {
-                new Contact { ContactId= 1,Name= "Aakash" },
-            new Contact { ContactId = 2, Name = "Aashay" }
-        };
-                          
+                contacts.Add(new Contact { ContactId = 1, Name = "Aakash" });
+            contacts.Add(new Contact { ContactId = 2, Name = "Aashay" });
+   
+
+            }
+            catch(Exception ex)
+            {
+                telemetryClient.TrackException(ex);
+            }
+                return name == null
+                    ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
+                    : req.CreateResponse(HttpStatusCode.OK, Newtonsoft.Json.JsonConvert.SerializeObject(contacts));
+            }
             
-            return name == null
-                ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
-                : req.CreateResponse(HttpStatusCode.OK, Newtonsoft.Json.JsonConvert.SerializeObject(contacts));
-        }
     }
 
-    public class Contact : IEnumerable
+    public class Contact 
     {
         public int ContactId { get; set; }
 
---
+++ TodoApp/TelemetryClientFactory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp
+{
+    public class TelemetryClientFactory : ITelemetryClientFactory
+    {
+        public virtual TelemetryClient GetClient()
+        {
+            string key = TelemetryConfiguration.Active.InstrumentationKey = System.Environment.GetEnvironmentVariable("APPINSIGHTS_INSTRUMENTATIONKEY", EnvironmentVariableTarget.Process);
+            TelemetryClient client = new TelemetryClient()
+            {
+                InstrumentationKey = key
+            };
+
+            return client;
+        }
+    }
+
+    public interface ITelemetryClientFactory
+    {
+        TelemetryClient GetClient();
+    }
+}
